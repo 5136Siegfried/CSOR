b2393c67b09f0a3e0966f23b8f849a91142d8a7d
int2cs <gabriel.gn87@protonmail.com>
2022-06-03 11:03:23 +0200

    initial commit

---
+++ pages/donation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="site">
+    <Header />
+    <h1>Nous équiper !</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Donation'
+}
+</script>
+
+<style scoped>
+
+</style>
