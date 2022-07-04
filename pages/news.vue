14a2ca0235ba6a0266c33df93d4ab0189243dd9e
int2cs <gabriel.gn87@protonmail.com>
2022-07-04 12:40:10 +0200

    MàJ page news

--- pages/news.vue
+++ pages/news.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="site">
     <div class="site__wrapper">
-      <h1>Les news !</h1>
+      <h1>Les <b>news !</b></h1>
+      <div class="content"><p>arrive très bientôt ;)</p></div>
     </div>
   </div>
 </template>
@@ -12,6 +13,21 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.site{
+  min-height: calc(100vh - 100px);
+}
+.site__wrapper{
+  height: calc(70vh - 100px);
+  .content{
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items : center;
+    p{
+      color: $blue;
+      font-size: clamp(2vh, 4rem, 2rem);
+    }
+  }
+}
 </style>
