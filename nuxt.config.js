8ddd4abd2113b8a6352463d202c2905a639137c3
int2cs <gabriel.gn87@protonmail.com>
2022-09-07 12:27:54 +0200

    add ogimage

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ export default {
           ' et de maintenir un réseau partenarial avec plusieurs associations caritatives, des établissements de' +
           ' soins et des travailleurs sociaux.' +
           'Au cours de l\'année, des professionnel.le.s du soin et de la santé mettent à disposition leurs compétences pour transmettre aux bénévoles du CSOR des notions importantes et utiles pour nos maraudes.' },
-      { hid: 'og:image', property: 'og:image', content: "/img/csor-ogimage.png"}
+      { hid: 'og:image', property: 'og:image', content: "~/assets/img/csor-ogimage.png"}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/logo2.svg' },
